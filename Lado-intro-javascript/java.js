3d85e50c6471dc2699d6eda7dfbed12d5e9350ba
Clayton-art <cslado.student@asiancollege.edu.ph>
2026-05-04 20:18:37 +0800

    Add files via upload

---
+++ Lado-intro-javascript/java.js
@@ -0,0 +1,29 @@
+var user_name;
+user_name=prompt("What is your name?");
+document.write("Welcome to my page "
+    + user_name + "!");
+
+alert( '"Clayton" is type: ' + typeof "Clayton" + "\n\n"
+        + "3.14 is type: " + typeof 3.14 + "\n\n"
+        + "false is type: " + typeof false ) ;
+
+var number1 = 34.289;
+var number2 = 123e-5;
+
+var name = "Clayton";
+var title = 'Student';
+
+var message = "Gooner";
+
+var condition1 = true;
+var condition2 = false;
+
+var myBool = true;
+var myString  = "true";
+
+var storage = "Clayton";
+storage = 98;
+
+if (confirm("Want to go to Disneyland?"))
+    document.location.href
+    ="http://park.hongkongdisneyland.com";
